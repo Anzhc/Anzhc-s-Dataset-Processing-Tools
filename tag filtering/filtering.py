a3a43e203760d688954ff91909ff1a8dbdd89fb9
Anzhc <133806049+Anzhc@users.noreply.github.com>
2023-06-16 02:08:09 +0400

    Fixed it, finally...
    So fucking stupid of me.

--- tag filtering/filtering.py
+++ tag filtering/filtering.py
@@ -2,7 +2,7 @@ from fileinput import filename
 import os
 from pydoc import doc
 
-userinput0 = float(input("What are we keeping today? 0 - general sfw dataset | 1 - general nsfw dataset | 2 - just informational duplication removal: "))
+userinput0 = float(input("What are we removing today? 0 - general sfw dataset | 1 - general nsfw dataset | 2 - just informational duplication removal: "))
 while userinput0 not in [0,1,2]:
    userinput0 = float(input("please, enter a valid option number: "))
 
@@ -13,19 +13,21 @@ elif userinput0 == 1:
 elif userinput0 == 2:
     wanted_tags_file = "./unwanted_zero.txt"
 
-def read_wanted_tags(file_path):
+def read_unwanted_tags(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
-        wanted_tags = [tag.strip().replace("_", " ") for tag in file]
-    return wanted_tags
-    
+        unwanted_tags = [tag.strip() for tag in file]
+    return unwanted_tags
 
-def keep_wanted_tags(tags, wanted_tags):
+def remove_unwanted_tags(tags, unwanted_tags):
     filtered_tags = []
     for tag in tags:
         tag = tag.replace("_", " ")
-        if tag in wanted_tags:
+        if tag not in unwanted_tags:
             filtered_tags.append(tag)
+            
     return filtered_tags
+   
+
 
 def remove_repeated_tags(tags):
     filtered_tags = []
@@ -42,27 +44,29 @@ def remove_repeated_tags(tags):
             filtered_tags.append(tag)
     return filtered_tags
 
-def process_tags_file(file_path, wanted_tags):
+def process_tags_file(file_path, unwanted_tags):
     with open(file_path, "r", encoding="utf-8") as file:
         tag_string = file.read().strip()
-    tags = [tag.strip() for tag in tag_string.replace("_", " ").split(",")]
+    tags = [tag.strip().replace("_", " ") for tag in tag_string.split(",")]
 
-    filtered_tags = keep_wanted_tags(tags, wanted_tags)
+    filtered_tags = remove_unwanted_tags(tags, unwanted_tags)
     filtered_tags = remove_repeated_tags(filtered_tags)
-    print(file, filtered_tags)
-
+    print("Processed unwanted tags:", filtered_tags)
     with open(file_path, "w", encoding="utf-8") as file:
         file.write(",".join(filtered_tags))
 
-def process_tags_files(input_folder, output_folder, wanted_tags_file):
-    wanted_tags = read_wanted_tags(wanted_tags_file)
+    return filtered_tags
+
+
+def process_tags_files(input_folder, output_folder, unwanted_tags_file):
+    unwanted_tags = read_unwanted_tags(unwanted_tags_file)
 
     for file_name in os.listdir(input_folder):
         if file_name.endswith(".txt"):
             input_file_path = os.path.join(input_folder, file_name)
             output_file_path = os.path.join(output_folder, file_name)
 
-            process_tags_file(input_file_path, wanted_tags)
+            process_tags_file(input_file_path, unwanted_tags)
 
             os.replace(input_file_path, output_file_path)
 
@@ -71,4 +75,5 @@ def process_tags_files(input_folder, output_folder, wanted_tags_file):
 if __name__ == "__main__":
     input_folder = "./input_tags"
     output_folder = "./output_tags"
-    process_tags_files(input_folder, output_folder, wanted_tags_file)
+    unwanted_tags_file = "./unwanted_tags.txt"
+    process_tags_files(input_folder, output_folder, unwanted_tags_file)
